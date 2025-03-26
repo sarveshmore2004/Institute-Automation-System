fd1e0aa8f67ae668371293387d6a8067a9808fc8
Divyansh Chandak <divyanshchandak2k20.4236@gmail.com>
2025-03-27 00:52:15 +0530

    add react-query

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,21 +2,45 @@ import './App.css';
 import ComplaintSection from './components/complaintSection';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import {
+    QueryClient,
+    QueryClientProvider,
+} from '@tanstack/react-query';
 
+const queryClient = new QueryClient()
 function App() {
-  return (
-    // <div className="App">
-    //   Institute Automation System
-    // </div>
-    <>
-    <Navbar/>
-    <div className='flex'>
-    <Sidebar/>
-    <ComplaintSection/>
-    </div>
-    
-    </>
-  );
+    const Layout = () => {
+        return (
+            <div className="app">
+            <QueryClientProvider client={queryClient}>
+                <Navbar />
+                <div style={{ display: 'flex' }}>
+                <Sidebar />
+                <div style={{ flexGrow: 1 }}>
+                    <Outlet />
+                </div>
+                </div>
+            </QueryClientProvider>
+            </div>
+        );
+    };
+
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Layout />,
+            children: [
+                {
+                    path: "/complaint",
+                    element: <ComplaintSection />,
+                },
+                
+            ],
+        },
+    ]);
+
+    return <RouterProvider router={router} />;
 }
 
 export default App;
