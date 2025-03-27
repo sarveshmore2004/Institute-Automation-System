3ed970eca0b4ebd6d760a6c204c8515e8d1eaf5c
Kevin Jacob <kevintj916@gmail.com>
2025-03-27 13:25:54 +0000

    made changes to check

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
-// import User from '../models/user.model.js';
-// import bcrypt from "bcrypt";
-// import jwt from "jsonwebtoken";
-// import createError from '../utils/createError.js';
+import User from '../models/user.model.js';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import createError from '../utils/createError.js';
 
 // export const register = async (req, res, next) => {
 //     try {
