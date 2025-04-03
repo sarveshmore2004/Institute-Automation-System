2232079b3d602e9d0cbc95bccfde19be3b3d2418
Divyansh Chandak <divyanshchandak2k20.4236@gmail.com>
2025-04-04 02:56:51 +0530

    ek baar ke liye user model daal diya

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({origin:"http://localhost:47223", credentials:true}));
+app.use(cors({origin:"http://localhost:3000", credentials:true}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(authRouter);
