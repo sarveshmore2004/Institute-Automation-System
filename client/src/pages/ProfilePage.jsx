a7f63d7d557852700591bf59fc899b2e75c29949
Divyansh Chandak <divyanshchandak2k20.4236@gmail.com>
2025-04-07 09:26:12 +0530

    minor

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,17 +1,3 @@
-// import React from "react";
-// import StudentProfile from "../components/ProfilePage/studentProfile.jsx";
-// import FacultyProfile from "../components/ProfilePage/facultyProfile.jsx";
-// import { useContext } from "react";
-// import { RoleContext } from "../context/Rolecontext.jsx";
-// const ProfilePage = ({ }) => {
-
-//   const { role } = useContext(RoleContext);
-
-//   return role === "student" ? <StudentProfile /> : <FacultyProfile />;
-// };
-
-// export default ProfilePage;
-
 import React, { useContext } from "react";
 import StudentProfile from "../components/ProfilePage/studentProfile.jsx";
 import FacultyProfile from "../components/ProfilePage/facultyProfile.jsx";
