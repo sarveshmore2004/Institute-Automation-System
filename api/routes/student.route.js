e3352160f8ad0eea462063e97a649fdb6c1b83d4
aditya kumar <adivbps@gmail.com>
2025-04-13 16:43:46 +0530

    integrated backend for the myCourses page and the drop courses page

--- api/routes/student.route.js
+++ api/routes/student.route.js
@@ -1,19 +1,23 @@
 import express from "express";
 import { 
     getStudent, 
+    getStudentCourses,
+    dropCourse,
     getStudentBonafideDetails, 
     createBonafideApplication,
     getBonafideApplications 
 } from "../controllers/student.controller.js";
 
-const router = express.Router();
 
-// Basic student routes
+const router = express.Router();
 router.get("/:id", getStudent);
+router.get("/:id/courses", getStudentCourses);
+router.delete("/:id/courses/:courseId",dropCourse);
 
 // Bonafide routes
 router.get("/:id/bonafide", getStudentBonafideDetails);
 router.post("/:id/bonafide/apply", createBonafideApplication);
 router.get("/:id/bonafide/applications", getBonafideApplications);
+
 
 export default router;
