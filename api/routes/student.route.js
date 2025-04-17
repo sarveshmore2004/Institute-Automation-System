90930ce0a719ff3c86eb4a97d8cd5078f3f3f80f
Kashish Aggarwal <a.kashish@iitg.ac.in>
2025-04-18 00:28:12 +0530

    transcript backend errors resolved

--- api/routes/student.route.js
+++ api/routes/student.route.js
@@ -1,35 +1,36 @@
 import express from "express";
-import { 
-    getStudent, 
-    getStudentCourses,
-    dropCourse,
-    getStudentBonafideDetails, 
-    createBonafideApplication,
-    getBonafideApplications,
-    getStudentPassportDetails,
-    submitPassportApplication,
-    getPassportApplications,
-    getCourseAnnouncements,
-    createCourseDropRequest,
-    getStudentDropRequests,
-    cancelDropRequest,
-    getCompletedCourses,
-    updateStudentProfile,
-    getAvailableCourses,
-    submitCourseApprovalRequest,
-    getPendingRequests,
-    getStudentFromRollNumber
+import {
+  getStudent,
+  getStudentCourses,
+  dropCourse,
+  getStudentBonafideDetails,
+  createBonafideApplication,
+  getBonafideApplications,
+  getStudentPassportDetails,
+  submitPassportApplication,
+  getPassportApplications,
+  getCourseAnnouncements,
+  createCourseDropRequest,
+  getStudentDropRequests,
+  cancelDropRequest,
+  getCompletedCourses,
+  updateStudentProfile,
+  getAvailableCourses,
+  submitCourseApprovalRequest,
+  getPendingRequests,
+  getStudentFromRollNumber,
+  getPerformance,
 } from "../controllers/student.controller.js";
 
 //courses routes
 const router = express.Router();
 router.get("/:id", getStudent);
 router.get("/:id/courses", getStudentCourses);
+router.get("/:id/performance", getPerformance);
 
 // router.delete("/:id/courses/:courseId", dropCourse);
-router.get('/courses/:courseId', getCourseAnnouncements);
-router.get("/:id/completed-courses", getCompletedCourses); 
-
+router.get("/courses/:courseId", getCourseAnnouncements);
+router.get("/:id/completed-courses", getCompletedCourses);
 
 // Course drop request routes
 router.post("/:id/drop-requests", createCourseDropRequest);
@@ -38,8 +39,7 @@ router.delete("/:id/drop-requests/:requestId", cancelDropRequest);
 
 // router.delete("/:id/courses/:courseId", dropCourse);
 router.put("/:id/profile", updateStudentProfile);
-router.get("/:id/rollno",getStudentFromRollNumber);
-
+router.get("/:id/rollno", getStudentFromRollNumber);
 
 // Bonafide routes
 router.get("/:id/bonafide", getStudentBonafideDetails);
@@ -47,10 +47,9 @@ router.post("/:id/bonafide/apply", createBonafideApplication);
 router.get("/:id/bonafide/applications", getBonafideApplications);
 
 // Passport routes
-router.get('/:id/passport', getStudentPassportDetails);
-router.post('/:id/passport/apply', submitPassportApplication);
-router.get('/:id/passport/applications', getPassportApplications);
-
+router.get("/:id/passport", getStudentPassportDetails);
+router.post("/:id/passport/apply", submitPassportApplication);
+router.get("/:id/passport/applications", getPassportApplications);
 
 // Course approval request routes
 
